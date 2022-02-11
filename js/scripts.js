1f6e73894fd11999ccf762fe3cee108bf78b3b07
Britt Boudreaux <br_boudreaux@msn.com>
2022-02-10 21:14:41 -0600

    Reservation Page

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    });
+     $("#reserveButton2").click(function () {
+       $("#reserveModal").modal("show");
+     });
+     $("#reserveButton3").click(function () {
+       $("#reserveModal").modal("show");
+     });
+     $("#reserveButton4").click(function () {
+       $("#reserveModal").modal("show");
+     });
+     $("#reserveButton5").click(function () {
+       $("#reserveModal").modal("show");
+     });
+     $("#reserveButton6").click(function () {
+       $("#reserveModal").modal("show");
+     });
+});
